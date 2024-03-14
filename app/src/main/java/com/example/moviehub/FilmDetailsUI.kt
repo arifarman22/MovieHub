0ca7ed20bde03560be38f9aad8e68b20b9007fb4
arifarman22 <pavel15-5555@diu.edu.bd>
2024-03-14 16:57:43 +0600

    This is a project of MovieSide App

---
+++ app/src/main/java/com/example/moviehub/FilmDetailsUI.kt
@@ -0,0 +1,11 @@
+package com.example.moviehub
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FilmDetailsUI : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_film_details_ui)
+    }
+}
